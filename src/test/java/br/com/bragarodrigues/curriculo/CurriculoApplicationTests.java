7e1d2dae2ce25dcb7dae7fac4ed363da8186a5ff
moreiraBraga <matheusmbraga94@gmail.com>
2022-10-24 10:06:15 -0300

    Projeto inicializado

---
+++ src/test/java/br/com/bragarodrigues/curriculo/CurriculoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bragarodrigues.curriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
